60a91e854fa33fd4f84f3db8486a5ef8496b1f34
Billy Nunnery <nunnerybilly40@gmail.com>
2019-11-17 21:09:46 -0600

    homework returned by manav and corrected by me

--- HW5.js
+++ HW5.js
@@ -1,17 +1,23 @@
-const initfizzbuzz = function(end,num1,num2){
-  let begin = (num1 * num2);
-    for (let i = 0; i <= end; i += 1) {
-      if (i % begin === 0) {
-        console.log("Fizzbuzz!");
-      } else if (i % num2 === 0) {
-        console.log("buzz");
-      } else if (i % num1 === 0) {
-        console.log("fizz");
-      } else {
-        console.log(i);
-      }
-    }
+const beginIterate = function(num1,num2){
+  beginIter= (num1*num2)
+  return beginIter};
 
-    return; // fizzbuzz46;
-  }
-  initfizzbuzz(24,6,4)
+  const SortFizz = function(begin,end,num1,num2){
+  for (let i = 0; i <= end; i += 1) {
+    if (i % begin === 0) {
+      console.log("Fizzbuzz!");
+    } else if (i % num2 === 0) {
+      console.log("buzz");
+    } else if (i % num1 === 0) {
+      console.log("fizz");
+    } else {
+      console.log(i);
+    }
+  }}
+
+const initfizzbuzz = function(end,num1,num2){
+ let begin = beginIterate(num1,num2);
+ let sort = SortFizz(begin,end,num1,num2);
+}
+
+initfizzbuzz(10,5,6);
